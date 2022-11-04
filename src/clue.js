578673dc002e28ae3aa81d0a221cabac9d632cd8
MartaFR2801 <martafrancoramos28@gmail.com>
2022-11-04 21:22:18 +0100

    cluedo hecho falta ejercicio 4 y 5 y un poco del 6

--- src/clue.js
+++ src/clue.js
@@ -1,26 +1,160 @@
 // ITERATION 1
 
 // Suspects Array
-
-const suspectsArray = [];
+const suspectsArray = [{
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+},
+{
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+}, {
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+}, {
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+}, {
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+}, {
+    firstName: "Marta",
+    lastName: "Franco",
+    occupation: "Teacher",
+    age: 25,
+    description: "brown curly hair",
+    image: "html",
+    color: "blue",
+}];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [{
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}, {
+    name: "Mrs.Lola",
+}
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [{
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}, {
+    name: "Carlos",
+    weight: 80
+}];
 
 
 // ITERATION 2
+/*
+array.length para saber si el array esta vacio. Si es mayor que cero, 
+significa que la array tiene algun valor. Math.random significa que tiene un valor aleatorio
+*/
+function selectRandom(array) {
+    if (array.length() === 0) {
+        return undefined;
+    }
+    else if (array.length() > 0) {
 
-function selectRandom() {}
+        return array[Math.random() * array.length()]; 
+    }
+}
+console.log(selectRandom(suspectsArray)); //const suspectArry que esta arriba del todo
 
-function pickMystery() {}
+/*  tengo que devolver un objeto y de ahi let object. y las tres propiedades */
+function pickMystery() {
+    let object = {
+        suspect: "",
+        weapon: "",
+        room: "",
+    }
+    /* aqui hay que darle un valor a cada propiedad. SelectRandom es 
+    seleccionar un elemento al azar de un array */
+    object.suspect = selectRandom(suspectsArray);
+    object.weapon = selectRandom(weaponsArray);
+    object.room = selectRandom(roomsArray);
+    //hay que hacer un return del objeto
+    return object;
+}
 
 
 // ITERATION 3
-
-function revealMystery() {}
-
+/* object va entre parentesis porque es el parametro de la funcion que hay que declarar.*/
+function revealMystery(object) {
+    return '${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon} in the ${object.place}!';
+}
+console.log(revealMystery(pickMystery()));
