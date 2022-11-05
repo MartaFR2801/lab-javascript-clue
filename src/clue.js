d2cb266d99bb4d99942e0a51791e47b83ac0d8ab
MartaFR2801 <martafrancoramos28@gmail.com>
2022-11-05 14:14:27 +0100

    clue finalizado

--- src/clue.js
+++ src/clue.js
@@ -125,36 +125,38 @@ array.length para saber si el array esta vacio. Si es mayor que cero,
 significa que la array tiene algun valor. Math.random significa que tiene un valor aleatorio
 */
 function selectRandom(array) {
-    if (array.length() === 0) {
+    if (array.length === 0) {
         return undefined;
     }
-    else if (array.length() > 0) {
 
-        return array[Math.random() * array.length()]; 
+
+        return array[Math.floor(Math.random() * array.length)]; 
     }
-}
+
 console.log(selectRandom(suspectsArray)); //const suspectArry que esta arriba del todo
 
 /*  tengo que devolver un objeto y de ahi let object. y las tres propiedades */
 function pickMystery() {
     let object = {
-        suspect: "",
-        weapon: "",
-        room: "",
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray),
     }
     /* aqui hay que darle un valor a cada propiedad. SelectRandom es 
-    seleccionar un elemento al azar de un array */
+    seleccionar un elemento al azar de un array 
     object.suspect = selectRandom(suspectsArray);
     object.weapon = selectRandom(weaponsArray);
     object.room = selectRandom(roomsArray);
     //hay que hacer un return del objeto
+    */
     return object;
 }
 
 
 // ITERATION 3
 /* object va entre parentesis porque es el parametro de la funcion que hay que declarar.*/
-function revealMystery(object) {
-    return '${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon} in the ${object.place}!';
+function revealMystery (reveal = pickMystery()) {
+    console.log(reveal)
+    return `${reveal.suspect.firstName} ${reveal.suspect.lastName} killed Mr. Boddy using the ${reveal.weapon.name} in the ${reveal.room.name}!`;
 }
-console.log(revealMystery(pickMystery()));
+
